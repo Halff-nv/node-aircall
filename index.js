9356c09df59047fe066b2efe87a991713b2ba8db
JonaHalffman <jona.halffman@gmail.com>
2024-04-16 12:19:41 +0200

    deploy init

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+app.use(bodyParser.json());
+
+const ENDPOINT_ET = "https://elektrotechnieker-backend-production.up.railway.app/";
+const AIRCALL_ROOT = "https://api.aircall.io/v1/";
+
+const api_id = "09b6995dd8b6f94922a2b7f9744aa8aa";
+const api_token = "5c25cd93d033e4dc99d87f6b954b33c8";
+
+
+// base 64 encode for basic auth
+const basic_auth = Buffer.from(api_id + ':' + api_token).toString('base64');
+
+
+
+
+
+app.post('/aircall/webhook', (req, res) => {
+    const callData = req.body;
+    console.log("callData", callData);
+
+
+
+    // const resource = callData.resource;
+
+
+
+
+
+
+    res.sendStatus(200);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+
+
+
+/**
+ * Show info card to user on call_id
+ */
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}`);
 });
