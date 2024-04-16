d1a4c6e1362dd93663469c01ec9a164924517fa2
JonaHalffman <jona.halffman@gmail.com>
2024-04-16 13:17:04 +0200

    deploy test

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-const ENDPOINT_ET = "https://elektrotechnieker-backend-production.up.railway.app/";
-const AIRCALL_ROOT = "https://api.aircall.io/v1/";
 
 const api_id = "09b6995dd8b6f94922a2b7f9744aa8aa";
 const api_token = "5c25cd93d033e4dc99d87f6b954b33c8";
@@ -19,28 +18,127 @@ const basic_auth = Buffer.from(api_id + ':' + api_token).toString('base64');
 
 
 
-app.post('/aircall/webhook', (req, res) => {
+app.post('/aircall/webhook', async (req, res) => {
     const callData = req.body;
-    console.log("callData", callData);
 
 
 
-    // const resource = callData.resource;
+    const event = callData.event;
+
+    if (event == "call.created") {
+        const call_id = callData.data.id;
+        const digits = callData.data.raw_digits;
+        console.log("Call created with call_id " + call_id + " and digits " + digits);
 
 
+        const info = await searchInfo(digits);
 
-
-
+        if (info == undefined || info?.length == 0) {
+            console.log("No info found for " + digits);
+            res.sendStatus(200);
+            return;
+        } else {
+            console.log("Info found for " + digits);
+            info.forEach((element) => createInfoCard(call_id, element))
+        }
+    }
 
     res.sendStatus(200);
 });
 
 
+/**
+ * Search info at our own back-end
+ */
+
+async function searchInfo(call_id) {
+    try {
+        // Parameters to be sent with the request
+        const params = {
+            telephone: call_id
+        };
+
+        // Create a URLSearchParams object from the params and automatically URL-encode them
+        const searchParams = new URLSearchParams(params);
+        const baseURL = 'https://elektrotechnieker-backend-production.up.railway.app/tickets/findByTelephone';
+
+        const fullURL = `${baseURL}?${searchParams.toString()}`;
+
+
+
+        // call our own back-end
+        const response = await axios.get(fullURL, {});
+        console.log(JSON.stringify(response.data));
+
+        // example return data:
+        // [
+        //     {
+        //         "id": 2272,
+        //         "name": " caby projects BV",
+        //         "location": "2620, Hemiksem",
+        //         "device": "Beko koel vies comb ",
+        //         "status": "request_ticket",
+        //         "opdrachtgever": "-"
+        //     }
+        // ]
+
+        return response.data;
+
+    }
+    catch (e) {
+        console.log(e);
+    }
+
+    return undefined;
+}
 
 
 /**
  * Show info card to user on call_id
  */
+async function createInfoCard(call_id, info_elem) {
+    try {
+        const response = await axios.post(`https://api.aircall.io/v1/calls/${call_id}/insight_cards`, {
+            contents: [
+                {
+                    type: "title",
+                    text: `Ticket ${info_elem?.id}`,
+                    link: `https://app.elektrotechnieker.be/dashboard/tickets/${info_elem?.id}/`
+                },
+                {
+                    type: "shortText",
+                    label: "Naam",
+                    text: info_elem?.name
+                },
+                {
+                    type: "shortText",
+                    label: "Locatie",
+                    text: info_elem?.location
+                },
+                {
+                    type: "shortText",
+                    label: "Toestel",
+                    text: `${info_elem?.device}`
+                },
+                {
+                    type: "shortText",
+                    label: "Opdrachtgever",
+                    text: `${info_elem?.opdrachtgever}`
+                }
+            ]
+        }, {
+            headers: {
+                Authorization: `Basic ${basic_auth}`
+            }
+        });
+
+        // console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
 
 
 app.listen(PORT, () => {
