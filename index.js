797a44612868c1acb62f4ca44aa01459e17b3689
JonaHalffman <jona.halffman@gmail.com>
2024-05-31 20:53:30 +0200

    Add & update contacts

--- index.js
+++ index.js
@@ -10,26 +10,17 @@ app.use(bodyParser.json());
 const api_id = "09b6995dd8b6f94922a2b7f9744aa8aa";
 const api_token = "5c25cd93d033e4dc99d87f6b954b33c8";
 
-
 // base 64 encode for basic auth
 const basic_auth = Buffer.from(api_id + ':' + api_token).toString('base64');
 
-
-
-
-
 app.post('/aircall/webhook', async (req, res) => {
     const callData = req.body;
-
-
-
     const event = callData.event;
 
     if (event == "call.created") {
         const call_id = callData.data.id;
         const digits = callData.data.raw_digits;
         console.log("Call created with call_id " + call_id + " and digits " + digits);
-
 
         const info = await searchInfo(digits);
 
@@ -39,18 +30,25 @@ app.post('/aircall/webhook', async (req, res) => {
             return;
         } else {
             console.log("Info found for " + digits);
-            info.forEach((element) => createInfoCard(call_id, element))
+            info.forEach((element) => {
+                createInfoCard(call_id, element);
+            });
+
+            // create or update contact in Aircall
+            try {
+                createOrUpdateContact(info[0]);
+            } catch (e) {
+                console.log(e);
+            }
         }
     }
 
     res.sendStatus(200);
 });
 
-
 /**
  * Search info at our own back-end
  */
-
 async function searchInfo(call_id) {
     try {
         // Parameters to be sent with the request
@@ -61,10 +59,7 @@ async function searchInfo(call_id) {
         // Create a URLSearchParams object from the params and automatically URL-encode them
         const searchParams = new URLSearchParams(params);
         const baseURL = 'https://elektrotechnieker-backend-production.up.railway.app/tickets/findByTelephone';
-
         const fullURL = `${baseURL}?${searchParams.toString()}`;
-
-
 
         // call our own back-end
         const response = await axios.get(fullURL, {});
@@ -83,15 +78,12 @@ async function searchInfo(call_id) {
         // ]
 
         return response.data;
-
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e);
     }
 
     return undefined;
 }
-
 
 /**
  * Show info card to user on call_id
@@ -138,13 +130,72 @@ async function createInfoCard(call_id, info_elem) {
         console.log(error);
         throw error;
     }
-};
+}
 
+/**
+ * Create or update contact in Aircall
+ */
+// TODO: consider both phone-numbers and the email_adres from the info_elem (send this too from back-end)
+// TODO: test if callData.data.contact is null
+async function createOrUpdateContact(info_elem, callData) {
+    try {
+
+        console.log("callData")
+        console.log(callData)
+
+        // retreive aircall contact from callData
+        if (callData.data.contact == null) {
+
+            // Create if contact does not exist
+            const responseCreate = await axios.post(`https://api.aircall.io/v1/contacts`, {
+                first_name: info_elem?.name,
+                last_name: info_elem?.opdrachtgever,
+                company_name: info_elem?.location,
+                information: `Ticket ${info_elem?.id} - ${info_elem?.device}`,
+                "phone_numbers": [
+                    {
+                        "label": "Default",
+                        "value": callData.data.raw_digits
+                    }
+                ],
+            }, {
+                headers: {
+                    Authorization: `Basic ${basic_auth}`
+                }
+            });
+
+            console.log("Contact creation response:", responseCreate.data);
+        } else {
+            // Update if contact exists
+            const responseUpdate = await axios.put(`https://api.aircall.io/v1/contacts/${callData.data.contact.id}`, {
+                first_name: info_elem?.name,
+                last_name: info_elem?.opdrachtgever,
+                company_name: info_elem?.location,
+                information: `Ticket ${info_elem?.id} - ${info_elem?.device}`,
+                "phone_numbers": [
+                    {
+                        "label": "Default",
+                        "value": callData.data.raw_digits
+                    }
+                ],
+            }, {
+                headers: {
+                    Authorization: `Basic ${basic_auth}`
+                }
+            });
+            console.log("Contact update response:", responseUpdate.data);
+
+            return true;
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server running at ${PORT}`);
 });
-
 
 // hello world
 app.get('/', (req, res) => {
